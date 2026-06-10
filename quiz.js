91357e59947825a948c2ad2d6ad15cd86d6d1904
Gaëtan Crabeels <92713802+GaetanCrabeels@users.noreply.github.com>
2026-06-10 17:03:41 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-
+let quizRef = null;
+let syncEnabled = false;
 const firebaseConfig = {
   apiKey: "AIzaSyBdCyzZpGL0lpHLJwVhZwa0Q4y9QFHFq58",
   authDomain: "totemolle.firebaseapp.com",
